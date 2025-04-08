5451506f65273d01ab1e0395ea7a3ae16d7d110f
DAVI SANDRO DOS SANTOS <davisantos003@cs.cruzeirodosul.edu.br>
2025-04-08 20:09:15 -0300

    fourth commit

--- terça/Ativ4.py
+++ terça/Ativ4.py
@@ -1,3 +1,10 @@
 #4- Faça um programa em Python que solicite ao usuário uma quantidade de segundos,
 #calcule e exiba a quantidade de horas, minutos e segundos.
 
+segundos_totais = int(input("Digite a quantidade de segundos: "))
+horas = segundos_totais // 3600
+segundos_restantes = segundos_totais % 3600
+minutos = segundos_restantes // 60
+segundos = segundos_restantes % 60
+print(f"\n{segundos_totais} segundos equivalem a:")
+print(f"{horas} horas, {minutos} minutos e {segundos} segundos")
